add7469dba078016d94c7dd3515b9af144f3aa00
Mikołaj Demków <mikolaj.demkow@codequest.com>
2021-08-06 14:34:30 +0200

    Change snake case to camel case

--- user/src/main/java/com/example/user/auth/data/dto/SessionDto.kt
+++ user/src/main/java/com/example/user/auth/data/dto/SessionDto.kt
@@ -6,6 +6,6 @@ import moe.banana.jsonapi2.Resource
 
 @JsonApi(type = "session")
 data class SessionDto(
-    @field:Json(name = "access_token") val access_token: String = "",
-    @field:Json(name = "refresh_token") val refresh_token: String = ""
+    @field:Json(name = "access_token") val accessToken: String = "",
+    @field:Json(name = "refresh_token") val refreshToken: String = ""
 ) : Resource()
